8b9b8ab34f604dc34cfce4c4484637f0175924d7
radu <chetreanalexandru63@gmail.com>
2025-09-27 22:10:56 +0200

    reg

---
+++ OOP.js
@@ -0,0 +1,84 @@
+const readline = require("node:readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function cl(text) {
+  console.log(text);
+}
+
+class User {
+  #name;
+  #age;
+  #password;
+
+  constructor(name = "", age = 0, password = "") {
+    this.#name = name;
+    this.#age = age;
+    this.#password = password;
+  }
+
+  getName() {
+    return new Promise((resolve) => {
+      rl.question("Write your name: ", (a) => {
+        if (typeof a === "string" && a.trim() !== "") {
+          this.#name = a;
+          cl(`${a} this is your name`);
+        } else {
+          cl("Write a correct name");
+        }
+        resolve();
+      });
+    });
+  }
+
+  getAge() {
+    return new Promise((resolve) => {
+      rl.question("Write your age: ", (a) => {
+        const ageNum = Number(a);
+        if (!isNaN(ageNum) && ageNum > 0) {
+          this.#age = ageNum;
+          cl(`${ageNum} is your age`);
+        } else {
+          cl("Write a correct age");
+        }
+        resolve();
+      });
+    });
+  }
+
+  getPas() {
+    return new Promise((resolve) => {
+      rl.question("Write your password (numbers only): ", (a) => {
+        const passNum = Number(a);
+        if (!isNaN(passNum)) {
+          this.#password = passNum;
+          cl(`${passNum} this is your password`);
+        } else {
+          cl("Write a correct password");
+        }
+        resolve();
+      });
+    });
+  }
+
+  Info() {
+    cl(`Name: ${this.#name}, Age: ${this.#age}, Password: ${this.#password}`);
+  }
+}
+
+const asyncFunc = async () => {
+  const user = new User();
+
+  await user.getName();
+  await user.getAge();
+  await user.getPas();
+
+  user.Info();
+
+  rl.close();
+};
+
+asyncFunc();
