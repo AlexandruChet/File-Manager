e7aca262eaeadc6bf80207103edba3b6ef456f98
radu <chetreanalexandru63@gmail.com>
2025-10-10 15:12:11 +0200

    algorithm

---
+++ binary-algorithm.js
@@ -0,0 +1,20 @@
+var binarySearchFn = function (array, target) {
+  var left = 0;
+  var right = array.length - 1;
+  while (left <= right) {
+    var mid = Math.floor((left + right) / 2);
+    var midValue = array[mid];
+    if (midValue === target) return mid;
+    else if (target < midValue) right = mid - 1;
+    else left = mid + 1;
+  }
+  return -1;
+};
+var sortedNumbers = [2, 5, 7, 8, 11, 12, 50, 89, 99, 23, 90].sort(function (
+  a,
+  b
+) {
+  return a - b;
+});
+var foundIndexInSorted = binarySearchFn(sortedNumbers, 11);
+console.log(foundIndexInSorted);
---
+++ binary-algorithm.ts
@@ -0,0 +1,21 @@
+const binarySearchFn = (array: number[], target: number): number => {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const midValue = array[mid];
+
+    if (midValue === target) return mid;
+    else if (target < midValue) right = mid - 1;
+    else left = mid + 1;
+  }
+
+  return -1;
+};
+
+const sortedNumbers = [2, 5, 7, 8, 11, 12, 50, 89, 99, 23, 90].sort(
+  (a, b) => a - b
+);
+const foundIndexInSorted = binarySearchFn(sortedNumbers, 11);
+console.log(foundIndexInSorted);
