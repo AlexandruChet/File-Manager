0fef500e2b6e3d073eb320ea043e650689356caa
radu <chetreanalexandru63@gmail.com>
2025-10-13 22:27:30 +0200

    debug

--- HTTP/api/rotate.js
+++ HTTP/api/rotate.js
@@ -1,17 +1,17 @@
 const rotate = (point, angle) => {
-  const sin = Math.sin(angle)
-  const cos = Math.sin(angle)
-  const { x, y } = point
-  point.x = x * cos - y * sin
-  point.y = x * sin + y * cos
-}
+  const sin = Math.sin(angle);
+  const cos = Math.cos(angle);
+  const { x, y } = point;
+  point.x = x * cos - y * sin;
+  point.y = x * sin + y * cos;
+};
 
 module.exports = async (name, angle) => {
-  const shape = memory.get(name)
-  if(!shape) return "Shape is not found"
+  const shape = memory.get(name);
+  if (!shape) return "Shape is not found";
   for (const key in shape) {
-    const point = shape[key]
-    rotate(point, angle)
+    const point = shape[key];
+    rotate(point, angle);
   }
-  return "shape is rotated"
-}
+  return "shape is rotated";
+};
